2addd2b675fbe410371924ecbc05f52b0454390b
Zain Uddin <a00279673@student.ait.ie>
2022-10-01 20:12:03 +0100

    added simple nodejs app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// Code a simple GET API
+// http://localhost:4300/simple-nodejs-app/api/v1/hello
+// https://zainuddin.com/simple-nodejs-app/api/v1/hello
+app.get('/simple-nodejs-app/api/v1/hello', function(req,res){
+    res.send(' Hello, We are learning Github Actions');
+})
+
+app.listen(4300, () => {
+    console.log('Server has started!')
+})
