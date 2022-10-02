c6e2460c514080664c9b3a6e1bd2fa52d48c2ada
Zain Uddin <a00279673@student.ait.ie>
2022-10-02 20:07:24 +0100

    added pm2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 // http://localhost:4300/simple-nodejs-app/api/v1/hello
 // https://zainuddin.com/simple-nodejs-app/api/v1/hello
 app.get('/simple-nodejs-app/api/v1/hello', function(req,res){
-    res.send(' Hello, We are learning Github Actions');
+    res.send(' Hello world');
 })
 
 app.listen(4300, () => {
